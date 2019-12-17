b51ab8b03cb9b0f379765aab158493faa7445053
Alberto Sánchez <albertoensan@gmail.com>
2019-12-17 02:08:19 +0100

    Interfaz terminada

--- ahorcado.py
+++ ahorcado.py
@@ -50,10 +50,39 @@ IMAGES = ['''
         |
         =========''','''
 
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''','''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''','''
+
 ''']
+
+WORDS = [
+    'lavadora',
+    'secadora',
+    'sofa',
+    'gobierno',
+    'democracia',
+    'dictadura',
+    'computadora',
+    'movil',
+    'virtual'
+]
 
 def main():
     pass
 
 if __name__ == '__main__':
+    print('BIENVENIDOS AL JUEGO DEL AHORCADO')
     main()
